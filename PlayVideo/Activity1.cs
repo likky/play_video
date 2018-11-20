cb029ff1ae393cb545ae047b03b2029e34376a94
likky <664907696@qq.com>
2018-11-20 12:20:20 +0800

    Add MediaController

--- PlayVideo/Activity1.cs
+++ PlayVideo/Activity1.cs
@@ -5,13 +5,16 @@ using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Media;
 using Android.OS;
 
 namespace PlayVideo
 {
     [Activity (Label = "PlayVideo", MainLauncher = true)]
-    public class Activity1 : Activity
+    public class Activity1 : Activity, MediaPlayer.IOnCompletionListener
     {
+
+
         protected override void OnCreate (Bundle bundle)
         {
             base.OnCreate (bundle);
@@ -24,8 +27,20 @@ namespace PlayVideo
             var uri = Android.Net.Uri.Parse ("http://ia600507.us.archive.org/25/items/Cartoontheater1930sAnd1950s1/PigsInAPolka1943.mp4");
             
             videoView.SetVideoURI (uri);
+            //本地播放
+            //videoView.SetVideoPath(path);
+            videoView.SetMediaController(new MediaController(this));
+            videoView.SetOnCompletionListener(this);
 			videoView.Visibility = ViewStates.Visible;
+            //开始播放视频
             videoView.Start ();   
+        }
+
+        public void OnCompletion(MediaPlayer mp)
+        {
+            Toast.MakeText(this, "播放完成了", ToastLength.Short).Show();
+            mp.Start();
+            mp.Looping = true;
         }
     }
 }
